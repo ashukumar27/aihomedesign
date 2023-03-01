a5292780b4096a8dfac6329fb8baebfb0f276428
Ashutosh Kumar <ashu.iitkgp@gmail.com>
2023-03-01 08:12:13 +0530

    pip3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install replicate
+pip3 install replicate
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
